4ef07fc65ec59e38422b487067fab8f16b53b2be
archandla <d.a.n.202021@gmail.com>
2023-10-22 18:22:12 +0300

    feat(SPRING-1): spring init
    actuator/health hand

---
+++ src/test/java/com/alsab/boozycalc/BoozycalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alsab.boozycalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoozycalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
