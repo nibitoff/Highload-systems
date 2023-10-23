bf5c8c8880cd82e98fb24879bdada89f7e23fdac
nibibee <72893410+nibitoff@users.noreply.github.com>
2023-10-23 12:03:15 +0300

    feat(db-1): ingredients and ingredients_type tables implemented

--- src/main/java/com/alsab/boozycalc/repository/IngredientRepo.java
+++ src/main/java/com/alsab/boozycalc/repository/IngredientRepo.java
@@ -1,10 +1,11 @@
 package com.alsab.boozycalc.repository;
 
 import com.alsab.boozycalc.entity.IngredientEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IngredientRepo extends CrudRepository<IngredientEntity, Long> {
+public interface IngredientRepo extends JpaRepository<IngredientEntity, Long> {
 
 }
