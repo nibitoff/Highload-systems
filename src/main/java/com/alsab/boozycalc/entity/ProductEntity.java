dc23029733a76f4a6baa388318f5c8aca125ef05
archandla <d.a.n.202021@gmail.com>
2023-10-28 20:34:45 +0300

    feat(SPRING-6): productentity with lombok

--- src/main/java/com/alsab/boozycalc/entity/ProductEntity.java
+++ src/main/java/com/alsab/boozycalc/entity/ProductEntity.java
@@ -1,8 +1,14 @@
 package com.alsab.boozycalc.entity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,54 +20,7 @@ public class ProductEntity {
     private String description;
     @ManyToOne
     @JoinColumn(name = "ingredient_id", referencedColumnName = "id")
-    private IngredientEntity ingredient_id;
+    private IngredientEntity ingredient;
     @Column(name = "price")
     private float price;
-
-    public ProductEntity(){
-
-    }
-
-    public ProductEntity(String name, String description, IngredientEntity ingredientEntity, float price) {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public IngredientEntity getIngredientId() {
-        return ingredient_id;
-    }
-
-    public void setIngredientId(IngredientEntity ingredient_id) {
-        this.ingredient_id = ingredient_id;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
 }
