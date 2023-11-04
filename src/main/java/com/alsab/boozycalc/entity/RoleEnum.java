e983c44f0e557663e05e51ebf827da894f62ad96
archandla <d.a.n.202021@gmail.com>
2023-11-04 11:57:26 +0300

    feat(SEC-1): drop userRoleEntity, using RoleEnum

---
+++ src/main/java/com/alsab/boozycalc/entity/RoleEnum.java
@@ -0,0 +1,8 @@
+package com.alsab.boozycalc.entity;
+
+public enum RoleEnum {
+    USER,
+    ADMIN,
+    ORGANIZER,
+    BARTENDER
+}
--- src/main/java/com/alsab/boozycalc/entity/UserEntity.java
+++ src/main/java/com/alsab/boozycalc/entity/UserEntity.java
@@ -1,61 +1,36 @@
 package com.alsab.boozycalc.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CollectionId;
+
+import javax.management.relation.Role;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "username")
     private String username;
+
     @Column(name = "password")
     private String password;
+
     @Column(name = "real_name")
     private String realName;
 
-
-
-
-    public UserEntity() {
-    }
-
-    public UserEntity(String userName, String password, String realName) {
-        this.username = userName;
-        this.password = password;
-        this.realName = realName;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return username;
-    }
-
-    public void setUserName(String userName) {
-        this.username = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRealName() {
-        return realName;
-    }
-
-    public void setRealName(String realName) {
-        this.realName = realName;
-    }
+    @ElementCollection(targetClass = RoleEnum.class)
+    @JoinTable(name = "user_to_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "roles", nullable = false)
+    @Enumerated(EnumType.STRING)
+    List<RoleEnum> roles;
 }
