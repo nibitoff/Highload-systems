7f30c825d59097230b0da1ce12238fd0562b17f8
archandla <d.a.n.202021@gmail.com>
2023-11-04 16:06:22 +0300

    feat(SEC-1): jwt auth and register

---
+++ src/main/java/com/alsab/boozycalc/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.alsab.boozycalc.controller;
+
+import com.alsab.boozycalc.security.payload.AuthenticationRequest;
+import com.alsab.boozycalc.security.payload.AuthenticationResponse;
+import com.alsab.boozycalc.security.payload.RegisterRequest;
+import com.alsab.boozycalc.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+}
