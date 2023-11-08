797242dcbd30ab1f7037329164f40b9d38832fce
archandla <d.a.n.202021@gmail.com>
2023-11-08 16:28:58 +0300

    feat(DB-6): ingredients with unique name

--- src/main/java/com/alsab/boozycalc/repository/IngredientRepo.java
+++ src/main/java/com/alsab/boozycalc/repository/IngredientRepo.java
@@ -2,10 +2,13 @@ package com.alsab.boozycalc.repository;
 
 import com.alsab.boozycalc.entity.IngredientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface IngredientRepo extends JpaRepository<IngredientEntity, Long> {
-
+    @Query(value = "SELECT * FROM ingredients WHERE NAME = ?1", nativeQuery = true)
+    Optional<IngredientEntity> findByName(String name);
 }
