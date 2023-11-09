3babe3a24f85a64a33134193272ff2136c6bb0fa
archandla <d.a.n.202021@gmail.com>
2023-11-10 01:37:42 +0300

    fix(TEST-8): optimize imports CocktailService

--- src/main/java/com/alsab/boozycalc/service/CocktailService.java
+++ src/main/java/com/alsab/boozycalc/service/CocktailService.java
@@ -1,17 +1,10 @@
 package com.alsab.boozycalc.service;
 
 import com.alsab.boozycalc.dto.CocktailDto;
-import com.alsab.boozycalc.entity.CocktailEntity;
-import com.alsab.boozycalc.repository.CocktailRepo;
 import com.alsab.boozycalc.service.data.CocktailDataService;
 import com.alsab.boozycalc.service.data.CocktailTypeDataService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.awt.*;
 
 @Service
 @RequiredArgsConstructor
