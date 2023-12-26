33158a421f129c9eb896b2d462f868e60dc59f24
archandla <d.a.n.202021@gmail.com>
2023-12-26 10:21:19 +0300

    feat(cocktail-service: migrate to gradle

--- cocktail-service/src/main/java/com/alsab/boozycalc/cocktail/feign/AuthServiceFeignClient.java
+++ cocktail-service/src/main/java/com/alsab/boozycalc/cocktail/feign/AuthServiceFeignClient.java
@@ -1,6 +1,6 @@
 package com.alsab.boozycalc.cocktail.feign;
 
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
